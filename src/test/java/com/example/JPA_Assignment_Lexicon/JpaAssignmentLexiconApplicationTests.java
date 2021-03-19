ccc09aaa9fe4402b8c2e5c61759e69df159b077c
indonswe <afc2002afc@hotmail.com>
2021-03-19 19:06:54 +0100

    Initial commit

---
+++ src/test/java/com/example/JPA_Assignment_Lexicon/JpaAssignmentLexiconApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JPA_Assignment_Lexicon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaAssignmentLexiconApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
